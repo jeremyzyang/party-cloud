00e10d7e34ef0ae91d3ff53106f5af84b74448a4
Jeremy Yang <jeremy.z.yang@hotmail.com>
2015-01-14 22:46:35 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,62 +1,46 @@
 #### ui script
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
-  
-  # Application title
-  titlePanel("Party Word Clouds"),
-  
-  p("Jeremy Yang"),
+  titlePanel("Party Word Clouds"), 
+  h5('Yang/Singh'),
   br(),
-  br(),
-  
-  
-  # Sidebar with a slider input for the number of bins
+  h4('Extract party manifesto and presidential speech text from American Presidency Project and compute word clouds for a chosen year.'),
   
   
   fluidRow(
     
-    column(1,p("")),
-    column(2,h5("Democrat Manifesto:")),
-    
+    column(5),
     column(2,
-           plotOutput('plot1')),
-    
-    column(1,p("")),
-    column(2,h5("Republican Manifesto:")),
-    
-    column(2,
-           plotOutput('plot2'))
-  ),
+           selectInput("select1",label=h4("Select A Year:"),choices=as.character(seq(1956,2012,4)),selected="2012")),
+    column(5)),
   
-  fluidRow(
-    
-    column(1,p("")),
-    column(2,h5("Democrat Speech:")),
-    column(2,
-           plotOutput('plot3')),
-    
-    column(1,p("")),
-    column(2,h5("Republican Speech:")),
-    column(2,
-           plotOutput('plot4'))
-  ),
-  
-  br(),
-  br(),
   hr(),
   
   fluidRow(
     
-    column(5,p("")),
-    column(2,
-           selectInput("select1",label=h5("select a year:"),choices=as.character(seq(1956,2012,4)),selected="2012")),
-    column(5,p(""))
+    column(2,h5("Democrat Manifesto")),
     
+    column(4,
+           plotOutput('plot1')),
     
+    column(4,
+           plotOutput('plot2')),
+    column(2,h5("Republican Manifesto"))
+  ),
+  
+  fluidRow(
+    
+    column(2,h5("Democrat Speech")),
+    column(4,
+           plotOutput('plot3')),
+    column(4,
+           plotOutput('plot4')),
+    column(2,h5("Republican Speech"))
+    
+  )  
     
   )
   
   
-))
+)
